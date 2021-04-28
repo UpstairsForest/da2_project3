790ab679abc2d5d3832c82d921a5c036b872779c
Nikita Batalov <67872768+UpstairsForest@users.noreply.github.com>
2021-04-29 00:12:32 +0200

    added divergent beam

--- boost.py
+++ boost.py
@@ -1,5 +1,5 @@
 import numpy as np
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 from math import sqrt
 from scipy.constants import c
 from library import v_kaon as kaon_v
@@ -9,12 +9,13 @@ from library import m_pp, m_np, m_k
 
 def pion_pair():
     positive_pion_fv = random_isotropic_rotation(pion_fv)
-    neutral_pion_fv = -1*positive_pion_fv # inverting everything, so that the momenta are correct
-    neutral_pion_fv[0] = m_k*c*c - positive_pion_fv[0] # fixing energy through energy conservation
-    positive_pion_velocity = momentum_to_velocity(boost(positive_pion_fv)[1:], m_pp)
-    neutral_pion_velocity = momentum_to_velocity(boost(neutral_pion_fv)[1:], m_np)
-    
+    neutral_pion_fv = -1 * positive_pion_fv  # inverting everything, so that the momenta are correct
+    neutral_pion_fv[0] = m_k * c * c - positive_pion_fv[0]  # fixing energy through energy conservation
+    positive_pion_velocity = gaussian_angle_rotation(momentum_to_velocity(boost(positive_pion_fv)[1:], m_pp))
+    neutral_pion_velocity = gaussian_angle_rotation(momentum_to_velocity(boost(neutral_pion_fv)[1:], m_np))
+
     return (positive_pion_velocity, neutral_pion_velocity)
+
 
 def random_isotropic_rotation(pion_fv):  # ~Marsaglia method, checked, all good
     fv = 1 * pion_fv
@@ -28,50 +29,82 @@ def random_isotropic_rotation(pion_fv):  # ~Marsaglia method, checked, all good
     fv[3] = fv[3] * (1 - 2 * (a * a + b * b))
     return fv
 
-def boost(fv): # checked, all good
+
+def boost(fv):
     beta = kaon_v / c
     gamma = 1 / sqrt(1 - beta * beta)
-    LT = np.array([[gamma, 0, 0, beta * gamma*c],
+    LT = np.array([[gamma, 0, 0, beta * gamma * c],
                    [0, 1, 0, 0],
                    [0, 0, 1, 0],
-                   [beta * gamma/c, 0, 0, gamma]])
+                   [beta * gamma / c, 0, 0, gamma]])
 
     return LT @ fv
 
+
 def momentum_to_velocity(p, m):
-    p_tot2 = np.sum(p*p) # total momentum squared
-    beta2 = p_tot2 / (m*m*c*c + p_tot2)
-    return p * sqrt(1 - beta2)/m
+    p_tot2 = np.sum(p * p)  # total momentum squared
+    beta2 = p_tot2 / (m * m * c * c + p_tot2)
+    return p * sqrt(1 - beta2) / m
+
+
+def gaussian_angle_rotation(v):
+    if v[0] != 0:
+        current_theta = np.arctan(v[1] / v[0])
+        if v[0] < 0 and v[1] < 0:
+            current_theta = np.pi + current_theta
+    else:
+        current_theta = np.pi / 2
+        if v[1] < 0:
+            current_theta = np.pi + current_theta
+    v = Rz(-current_theta) @ v  # rotate to xz plane
+    if v[2] != 0:
+        current_phi = np.arctan(v[0] / v[2])
+        if v[2] < 0 and v[0] < 0:
+            current_phi = np.pi + current_phi
+    else:
+        current_phi = np.pi / 2
+        if v[0] < 0:
+            current_phi = np.pi + current_phi
+    v = Ry(-current_phi) @ v  # rotate to be parallel to z
+    '''
+    can't just set v[2] = |v| as we need to find the current theta and phi to put everything back
+    '''
+    rand_phi = np.random.normal(scale=0.001)
+    rand_theta = 2 * np.pi * np.random.rand()
+    return Rz(current_theta) @ Ry(current_phi) @ Rz(rand_theta) @ Ry(rand_phi) @ v
+
+
+def Rz(theta):
+    return np.array([[np.cos(theta), -np.sin(theta), 0],
+                     [np.sin(theta), np.cos(theta), 0],
+                     [0, 0, 1]])
+
+
+def Ry(phi):
+    return np.array([[np.cos(phi), 0, np.sin(phi)],
+                     [0, 1, 0],
+                     [-np.sin(phi), 0, np.cos(phi)]])
+
 
 pion_E = sqrt(m_pp * c * c * m_pp * c * c + pion_p * c * pion_p * c)
 pion_fv = np.array([pion_E, 0, 0, pion_p])
 
 """
-data_len = 100
-pions = np.zeros((data_len, 2, 3))
+data_len = 10000
+things = np.zeros((data_len, 3))
 
-shit = 0
+fig, ax = plt.subplots()
 
 for i in range(data_len):
-    v = pion_pair()
-    pions[i] = v
-    absolute_v = sqrt(v[0][0]**2 + v[0][1]**2 + v[0][2]**2), sqrt(v[1][0]**2 + v[1][1]**2 + v[1][2]**2)
-    if absolute_v[0] >= c:
-        shit +=1
-    if absolute_v[1] >= c:
-        shit +=1
-print("ratio of pions which exceed the speed of light:", round(shit/2/data_len, 2))
-
-fig, ax = plt.subplots(1, 1)
-for v in pions:
-    ax.plot((0, v[0, 2]), (0, v[0, 1]), lw=0.2, marker=".", ms=3, color="orange", alpha=0.5)
-    ax.plot((0, v[1, 2]), (0, v[1, 0]), lw=0.2, marker=".", ms=3, color="green", alpha=0.5)
+    v = gaussian_angle_rotation(pion_pair()[0])
+    things[i] = v
+    ax.plot((0, v[0]), (0, v[1]), ls = "", marker=".", ms=3, color="orange", alpha=400/data_len)
 
 ax.grid()
-ax.set_xlabel("v_z [m/s]")
-ax.set_ylabel("v_y [m/s]")
-ax.set_title("side_view")
-#ax.set_aspect("equal")
+ax.set_xlabel("x")
+ax.set_ylabel("y")
+ax.set_title("front_view")
+ax.set_aspect("equal")
 fig.tight_layout()
-plt.show() # +pi in orange, 0pi in green
+plt.show()
 """
