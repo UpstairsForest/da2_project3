aac0671bfae2af7dee4c14b5beb49b92f7ef3f5a
Nikita Batalov <67872768+UpstairsForest@users.noreply.github.com>
2021-04-29 11:41:13 +0200

    changed polynomial from 2nd to 4th

--- main.py
+++ main.py
@@ -70,31 +70,32 @@ average_fails_over_distances = 0*detector_positions
 fails_over_distances_over_iterations = np.zeros((iterations, len(detector_positions)))
 
 for i in range(iterations):
+    print("iteration:", i)
     decay_position, positive_pion_velocity, neutral_pion_velocity = data_generator()
     for j, d in enumerate(detector_positions):
         fails_over_distances_over_iterations[i, j] = number_of_detections(d)
-    plt.plot(detector_positions, fails_over_distances_over_iterations[i], lw = 1, color = "green", alpha = 1/iterations)
+    plt.plot(detector_positions[20:35], fails_over_distances_over_iterations[i][20:35], lw = 1, color = "green", alpha = 1/iterations)
 average_fails_over_distances = np.average(fails_over_distances_over_iterations, axis = 0) # axis 0 is iterations
 
 
-def model_fct(x, a, b, c):
-    return a*x**2 + b*x + c     # we want the fit to be a polynomial of second degree
+def model_fct(x, a, b, c, d, e):
+    return a*x*x*x*x + b*x*x*x + c*x*x + d*x + e     # we want the fit to be a polynomial of fourth degree
 
 
-detector_positions_slice = detector_positions[25:40]                        #slicing the data for a good fit
-average_fails_over_distances_slice = average_fails_over_distances[25:40]
+detector_positions_slice = detector_positions[20:35]                        #slicing the data for a good fit
+average_fails_over_distances_slice = average_fails_over_distances[20:35]
 popt, pcov = scipy.optimize.curve_fit(model_fct, detector_positions_slice, average_fails_over_distances_slice)  # fitting
 
 
 def fit(x):
-    return model_fct(x,popt[0], popt[1], popt[2])
+    return model_fct(x,popt[0], popt[1], popt[2], popt[3], popt[4])
 
 
 optimal = scipy.optimize.minimize(fit,x0=300)   # optimising the fit
 print("The minimum lies at", optimal["x"][0])
 
-plt.plot(detector_positions, average_fails_over_distances, lw = 1, marker = ".", color = "orange")
-#plt.plot(detector_positions_slice, model_fct(detector_positions_slice, popt[0], popt[1], popt[2]))
+plt.plot(detector_positions[20:35], average_fails_over_distances[20:35], lw = 1, marker = ".", color = "orange")
+plt.plot(detector_positions_slice, model_fct(detector_positions_slice, popt[0], popt[1], popt[2], popt[3], popt[4]), color = "black")
 plt.title("Number of Fails versus Detector Positions")
 plt.xlabel("Detector position")
 plt.ylabel("Number of fails")
